cb1f68194fef145580ab09c4bcdf7d3b4fe180fd
inshamanowar <inshamanowar22@gmail.com>
2022-01-04 18:38:43 +0530

    change debug

--- pmv/pmv/settings.py
+++ pmv/pmv/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cimtu-f^omyw3aw5!=&f#+a40f(o40*325zxv7)_+j+9d44n1h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pmvelectricinc.com','127.0.0.1']
 
 
 # Application definition
@@ -132,17 +132,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 X_FRAME_OPTIONS ="SAMEORIGIN"
